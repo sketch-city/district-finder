6d03758711e5e77e430d522c2e112f031ef8de06
Andrew Douglass <andrew@dou.glass>
2016-05-31 18:18:08 -0500

    Added view helper for text formatting.

---
+++ app/views/helpers/textFormatting.js
@@ -0,0 +1,27 @@
+/**
+ * Text formatting helper library.
+ * Helper methods to deal with adjacency lists and trees.
+ */
+var textFormatting = {
+
+
+  /**
+   * Adds left padding to format the text for hierarchical select input fields.
+   *
+   * @param {string} text - The text to add padding to.
+   * @param {number} depth - How deep is this item? Defines the amount of padding to add.
+   * @param {string} [spacer=" "] - What to fill it with. Defaults to a space.
+   */
+  formatDepth: function(text, depth, spacer) {
+    if (depth) {
+      spacer = spacer || " ";
+      var formatted = Array((depth*2) + 1).join(spacer);
+      text = formatted + text;
+    }
+    return text;
+  }
+
+
+};
+
+module.exports = textFormatting;
