1b3862218396ebfffcee12807fbc8a568582c28b
Andrew Douglass <andrew@dou.glass>
2016-10-14 12:21:14 -0500

    Added some small, bad documentation.

--- app/routers/api.js
+++ app/routers/api.js
@@ -8,7 +8,29 @@ var controllers = require('../controllers');
 var api = express.Router();
 
 // Build the routes
-api.get('/', function(req, res) { res.send("Wait! It's not documented yet. Check out what the example is doing."); });
+api.get('/', function(req, res) {
+  res.send(`
+    Sorry for the lame in a hurry documentation!
+    <br><br>
+    Fetch a list of districts the point is inside of:
+    <br>
+    <code>https://districtfinder.texasvotes.io/api/geo/{latitude}/{longitude}</code>
+    <br><br>
+    Example Output:
+    <pre>
+[
+  {
+    name: "0016",
+    type: "County Election Precinct"
+  },
+  {
+    name: "Harris",
+    type: "County"
+  }
+]
+    </pre>
+  `);
+});
 api.get('/geo/:lat/:lon', controllers.DistrictsController.allByLatLon);
 api.get('/regions', controllers.DistrictsController.getRegions);
 api.get('/region-types', controllers.DistrictsController.getRegionTypes);
